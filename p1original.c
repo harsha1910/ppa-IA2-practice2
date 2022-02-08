b932cba9d346b50fe9929b7819ab4b791ff779f0
C HARSHA VARDHAN REDDY <93115043+harsha1910@users.noreply.github.com>
2022-02-08 04:29:21 +0000

    committed

--- p1original.c
+++ p1original.c
@@ -1,2 +1,23 @@
-
+#include <stdio.h>
+void input(float *base,float *height)
+{
+ printf("enter the base and height\n");
+scanf("%f",base);
+scanf("%f",height);
+}
+void find_area(float base,float height,float*area)
+{
+  *area=((base*height)/2);
+}
+void output(float base,float height,float area)
+{
+  printf("area of %f  and %f is %f",base,height,area);
+}
+int main()
+{
+  float area,base,height;
+  input(&base,&height);
+  find_area(base,height,&area);
+  output(base,height,area);
+  return 0;
 }
